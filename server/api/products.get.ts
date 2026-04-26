631de7965d9989460bb225fda09b3bcdc02e9959
katarzynaotto <katarzyna.otto@movecloser.pl>
2026-04-26 10:45:23 +0200

    change file location

--- server/api/products.get.ts
+++ server/api/products.get.ts
@@ -6,7 +6,8 @@ import Papa from 'papaparse'
 import type { Product } from '~/types/product'
 
 export default defineEventHandler((): Product[] => {
-  const filePath = resolve('public/products.csv')
+  const filePath = resolve(process.cwd(), 'server/data/products.csv')
+
   const file = readFileSync(filePath, 'utf-8')
 
   const { data } = Papa.parse(file, {
